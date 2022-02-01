400087e651ae295316898a7f43c2c2a277b55d8b
Mike Greber <mikegreberdevelopment@gmail.com>
2022-02-01 09:34:07 -0700

    Enemy behaviour (#6)
    * add .idea to .gitignore
    
    * basic enemy script and prefab

---
+++ Descension/Assets/Scripts/Actor/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Actor.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float movementSpeed = 1;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+
+            if (x != 0 || y != 0)
+            {
+                transform.Translate(x * movementSpeed * Time.deltaTime,  y * movementSpeed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
