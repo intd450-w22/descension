74d01da4d286cf033cffc34f7717b7d2d3c81ec1
Mike Greber <mikegreberdevelopment@gmail.com>
2022-01-31 23:59:05 -0700

    basic enemy script and prefab

---
+++ Descension/Assets/Scripts/Actor/AI/AIController.cs
@@ -0,0 +1,246 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+namespace Actor.AI
+{
+    public class AIController : MonoBehaviour
+    {
+        public GameObject currentTarget;    // must set to object to be used as target
+        
+        public StateAttributes patrollingAttributes; // attributes when in patrolling state
+        public StateAttributes chasingAttributes;   // attributes when in chasing state
+    
+        public bool loopTargets;    // go to first target at end of list? or turn around if false
+        public State currentState;
+        public List<GameObject> patrolTargets;
+    
+        // internal state
+        private bool _paused;
+        private float _pausedTime = 0;
+        private Vector2 _forward;               // forward facing direction calculated from current movement direction
+        private int _patrolIndex = 0;           // index of current patrol target
+        private int _patrolDirection = 1;       // tracks forward/backward for patrolling
+        private StateAttributes _attributes;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartPatrol();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // line trace forward to look for player
+            See();
+            HandlePause();
+            if (!_paused) MoveToTarget();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log("Collision!!");
+            TurnAround();
+            _paused = true;
+        }
+
+        private void HandlePause()
+        {
+            if (_paused && _pausedTime < _attributes.pauseTime)
+            {
+                _pausedTime += Time.deltaTime;
+            }
+            else
+            {
+                _pausedTime = 0;
+                _paused = false;
+            }
+        }
+
+        // Starts chase state
+        private void StartChase(GameObject obj)
+        {
+            currentState = State.Chasing;
+            _attributes = chasingAttributes;
+            SetTarget(obj);
+        }
+
+        // Starts patrolling state. If no parameter input, goes to closest patrol target.
+        private void StartPatrol(int index = -1)
+        {
+            currentState = State.Patrolling;
+            _attributes = patrollingAttributes;
+            if (index != -1)
+            {
+                _patrolIndex = index;
+            }
+            else
+            {
+                _patrolIndex = Util.Util.FindClosest(transform.position, ref patrolTargets);
+            }
+            Debug.Log(_patrolIndex);
+            SetTarget(patrolTargets[_patrolIndex]);
+        }
+    
+        // Raycast forward to look for player, sets player as target if detected
+        private void See()
+        {
+            // ignore enemy layer 
+            RaycastHit2D rayCast = Physics2D.BoxCast(transform.position, new Vector2(2, 2), 0, _forward, _attributes.sightDistance, (int) ~Layer.Enemy);
+            if (rayCast)
+            {
+                if (rayCast.collider.gameObject.CompareTag("Player"))
+                {
+                    StartChase(rayCast.collider.gameObject);
+                    Debug.DrawRay(transform.position, _forward * _attributes.sightDistance, Color.red);
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, _forward * _attributes.sightDistance, Color.yellow);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, _forward * _attributes.sightDistance, Color.green);
+            }
+        }
+    
+        private void Hear()
+        {
+            // TODO implement hearing
+        }
+
+        // Look in all directions to see if player is still visible
+        private void Look()
+        {
+            foreach (var d in Util.Util.Directions)
+            {
+                RaycastHit2D rayCast = Physics2D.BoxCast(transform.position, new Vector2(2, 2), 0, d, _attributes.sightDistance, (int) ~Layer.Enemy);
+                if (rayCast)
+                {
+                    if (rayCast.collider.gameObject.CompareTag("Player"))
+                    {
+                        Debug.DrawRay(transform.position, d * _attributes.sightDistance, Color.red, 1);
+                        Debug.Log("Target found!");
+
+                        StartChase(rayCast.rigidbody.gameObject);
+                        return;
+                    }
+                    else
+                    {
+                        Debug.DrawRay(transform.position, d * _attributes.sightDistance, Color.yellow, 1);
+                    }
+                }
+                else
+                {
+                    Debug.DrawRay(transform.position, d * _attributes.sightDistance, Color.green, 1);
+                }
+            
+            }
+        
+            // patrol if player not found
+            Debug.Log("Target lost");
+            StartPatrol();
+        }
+    
+    
+        // sets target to position of obj and sets facing vector
+        private void SetTarget(GameObject obj)
+        {
+            currentTarget.transform.SetPositionAndRotation(obj.transform.position, obj.transform.rotation);
+            _forward = (currentTarget.transform.position - transform.position).normalized;
+            // Debug.Log(_forward);
+        }
+    
+        // process movement towards current target location
+        private void MoveToTarget()
+        {
+        
+            Vector3 difference = currentTarget.transform.position - transform.position;
+            if (difference.magnitude > 0.1)
+            {
+                transform.Translate(difference.normalized * _attributes.speed * Time.deltaTime);
+            }
+            else
+            {
+                OnTargetReached();
+            }
+        }
+
+        // executed when the target is reached
+        private void OnTargetReached()
+        {
+            switch (currentState)
+            {
+                case State.Patrolling:
+                {
+                    _paused = true;
+                    GetNextTarget();
+                    break;
+                }
+
+                case State.Chasing:
+                {
+                    Look();
+                    break;
+                }
+
+                case State.Attacking:
+                {
+                
+                    break;
+                }
+
+                default:
+                {
+                    break;
+                }
+            }
+        }
+
+        // sets target to next in patrolTargets list
+        private void GetNextTarget()
+        {
+            _patrolIndex += _patrolDirection;
+            if (loopTargets)
+            {
+                if (_patrolIndex == patrolTargets.Count) _patrolIndex = 0;
+                else if (_patrolIndex == -1) _patrolIndex = patrolTargets.Count - 1;
+            }
+            else
+            {
+                if (_patrolIndex == patrolTargets.Count || _patrolIndex == -1)
+                {
+                    _patrolDirection = -_patrolDirection;
+                    _patrolIndex += 2 * _patrolDirection;
+                }
+            }
+            Debug.Log(_patrolIndex);
+            SetTarget(patrolTargets[_patrolIndex]);
+        }
+    
+        private void TurnAround()
+        {
+            _patrolDirection = -_patrolDirection;
+            GetNextTarget();
+        }
+    }
+
+    public enum State
+    {
+        Patrolling,
+        Chasing,
+        Attacking
+    }
+
+    [Serializable]
+    public struct StateAttributes
+    {
+        public float speed;         // how fast to move in this state
+        public float sightDistance; // how far is vision in this state
+        public float pauseTime;     // how long to pause when current target is reached
+    }
+}
--- Descension/Assets/Scripts/Util/Util.cs
+++ Descension/Assets/Scripts/Util/Util.cs
@@ -1,18 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Util : MonoBehaviour
+namespace Util
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Util
     {
-        
+        static public readonly List<Vector2> Directions = new List<Vector2>()
+        {
+            new Vector2(1,0),
+            new Vector2(0,1),
+            new Vector2(-1,0),
+            new Vector2(0,-1),
+        };
+    
+        public static float DistanceSq(Vector2 a, Vector2 b)
+        {
+            return (b - a).sqrMagnitude;
+        }
+    
+        public static int FindClosest(Vector2 pos, ref List<GameObject> objects)
+        {
+            int closestIndex = 0;
+            float closestDistance = DistanceSq(pos, objects[0].transform.position);
+            for (int i = 1; i < objects.Count; ++i)
+            {
+                float dist = DistanceSq(pos, objects[i].transform.position);
+                if (dist < closestDistance)
+                {
+                    closestDistance = dist;
+                    closestIndex = i;
+                }
+            }
+            return closestIndex;
+        }
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    public enum Layer : int
     {
-        
+        Default       = 1 << 0,
+        TransparentFX = 1 << 1,
+        IgnoreRaycast = 1 << 2,
+        Player        = 1 << 3,
+        Water         = 1 << 4,
+        UI            = 1 << 5,
+        Enemy         = 1 << 6,
     }
 }
