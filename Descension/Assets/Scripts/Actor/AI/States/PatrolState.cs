a04bbcb57829c0ccab7fede139cbe30daadd5ebc
Mike Greber <mikegreberdevelopment@gmail.com>
2022-02-16 20:06:40 -0700

    Add enemies, reorganized some prefabs

--- Descension/Assets/Scripts/Actor/AI/States/PatrolState.cs
+++ Descension/Assets/Scripts/Actor/AI/States/PatrolState.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Diagnostics;
 using Util.Enums;
@@ -10,9 +11,11 @@ namespace Actor.AI.States
         public AIState onPlayerSpotted;
         public float sightDistance = 10;
         public float speed = 10;
+        public float reachThreshold = 3;
         public bool loopTargets;                // go to first target at end of list? or turn around if false
-
-        private Transform _currentTarget;
+        
+        
+        [ReadOnly] public Transform currentTarget;
         private int _patrolIndex = 0;           // index of current patrol target
         private int _patrolDirection = 1;       // tracks forward/backward for patrolling
         private List<Transform> _patrolTargets; // list of patrol targets, generated from children of PatrolTargets subobject
@@ -21,8 +24,15 @@ namespace Actor.AI.States
         {
             base.Start();
             _patrolTargets = new List<Transform>(transform.Find("PatrolTargets").GetComponentsInChildren<Transform>());
+            _patrolTargets.Remove(transform.Find("PatrolTargets"));
+            
+            Debug.Log("Count " +   _patrolTargets.Count);
+            foreach (Transform t in _patrolTargets)
+            {
+                Debug.Log(t.name);
+            }
             _patrolIndex = FindClosest(Controller.position, ref _patrolTargets);
-            _currentTarget = _patrolTargets[_patrolIndex];
+            currentTarget = _patrolTargets[_patrolIndex];
         }
 
         public override void Initialize()
@@ -52,15 +62,9 @@ namespace Actor.AI.States
                 Debug.DrawRay(Controller.position, Controller.agent.velocity.normalized * sightDistance, Color.green);
             }
         
-            float distance = (_currentTarget.position - Controller.position).magnitude;
-            if (distance > 2)
-            {
-                Controller.agent.SetDestination(_currentTarget.position);
-            }
-            else
-            {
-                GetNextTarget();
-            }
+            if ((currentTarget.position - Controller.position).magnitude < reachThreshold) GetNextTarget();
+
+            Controller.agent.SetDestination(currentTarget.position);
         }
     
         // sets target to next in patrolTargets list
@@ -81,7 +85,7 @@ namespace Actor.AI.States
                 }
             }
             
-            _currentTarget = _patrolTargets[_patrolIndex];
+            currentTarget = _patrolTargets[_patrolIndex];
         }
     }
 }
