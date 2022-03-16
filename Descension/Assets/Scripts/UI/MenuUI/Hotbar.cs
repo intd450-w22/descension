9b2c1f143c1cd8502e47fb501a829801997bfe4c
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-03-15 22:58:12 -0600

    Add Hotbar Outline

--- Descension/Assets/Scripts/UI/MenuUI/Hotbar.cs
+++ Descension/Assets/Scripts/UI/MenuUI/Hotbar.cs
@@ -1,33 +1,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using Items.Pickups;
+using UI.MenuUI;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Hotbar : MonoBehaviour
 {
-    private List<Image> hotbarSlots;
+    private List<HotbarSlot> _hotbarSlots;
 
     void Awake()
     {
-        hotbarSlots = GetComponentsInChildren<Image>().ToList();
+        _hotbarSlots = GetComponentsInChildren<HotbarSlot>().ToList();
+    }
+
+    void Start()
+    {
+        // Deactivate all slots
+        SetActive(-1);
     }
 
     public void SetActive(int slot)
     {
-        // TODO: Add a highlight around the inventory slot
+        for (var i = 0; i < _hotbarSlots.Count; i++)
+        {
+            if (i == slot)
+                _hotbarSlots[i].Activate();
+            else
+                _hotbarSlots[i].Deactivate();
+        }
     }
 
     public void PickupItem(EquippableItem item, int slot)
     {
         // Set sprite and quantity
-        hotbarSlots[slot].sprite = item.inventorySprite;
+        _hotbarSlots[slot].ItemSprite.sprite = item.inventorySprite;
+        // _hotbarSlots[slot].Quantity = item.Durability; TODO Get this
     }
 
     public void DropItem(int slot)
     {
         // Clear sprite and quantity
-        hotbarSlots[slot].sprite = null;
+        _hotbarSlots[slot].ItemSprite.sprite = null;
     }
 
 }
---
+++ Descension/Assets/Scripts/UI/MenuUI/HotbarSlot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Util.Helpers;
+
+namespace UI.MenuUI
+{
+    public class HotbarSlot : MonoBehaviour
+    {
+        [SerializeField]
+        private Color ActiveColour;
+        [SerializeField]
+        private Color InactiveColour;
+
+        public Image ItemSprite { get; set; }
+        public Image OutlineSprite { get; set; }
+        public int Quantity { get; set; } = -1;
+
+        void Awake()
+        {
+            ItemSprite = gameObject.GetChildObjectWithName("SlotItem").GetComponent<Image>();
+            OutlineSprite = gameObject.GetChildObjectWithName("SlotOutline").GetComponent<Image>();
+        }
+
+        public void Activate()
+        {
+            OutlineSprite.color = ActiveColour;
+        }
+
+        public void Deactivate()
+        {
+            OutlineSprite.color = InactiveColour;
+        }
+    }
+}
