0a3a3d21fa36b79d967323a479fcde62d9aeebb2
Mike Greber <mikegreberdevelopment@gmail.com>
2022-03-29 19:44:55 -0600

    Add Quality of Life Adjustments (#71)
    * Pause Background Audio in Pause Menu
    
    * Add OnLevelStart
    
    * Increase Sword Distance
    
    * Only Show Torch Description Once
    
    * Update tests.yml
    
    * Update Rock
    
    * Add Properly Sized TIlemap for Later
    
    * Add check in follow camera
    
    Co-authored-by: reillykeele <54957139+reillykeele@users.noreply.github.com>

--- Descension/Assets/Scripts/Managers/SoundManager.cs
+++ Descension/Assets/Scripts/Managers/SoundManager.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using Util.Audio;
 
 namespace Managers
 {
     public class SoundManager : MonoBehaviour
     {
-        public AudioSource removeRockSound;
-        public AudioSource goldFoundSound;
-        public AudioSource itemFoundSound;
-        public AudioSource arrowAttackSound;
-        public AudioSource inspectionSound;
-        public AudioSource errorSound;
-        
+        [Header("Configuration")]
+        [SerializeField] private float _fadeInTime;
+        [SerializeField] private float _fadeOutTime;
+
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource backgroundAudio;
+        [SerializeField] private AudioSource removeRockSound;
+        [SerializeField] private AudioSource goldFoundSound;
+        [SerializeField] private AudioSource itemFoundSound;
+        [SerializeField] private AudioSource arrowAttackSound;
+        [SerializeField] private AudioSource inspectionSound;
+        [SerializeField] private AudioSource errorSound;
+
         private static SoundManager _instance;
         private static SoundManager Instance
         {
@@ -28,35 +35,33 @@ namespace Managers
             DontDestroyOnLoad(gameObject);
         }
 
-        public static void RemoveRock() => Instance._RemoveRock();
-        private void _RemoveRock() 
+        public static void StartBackgroundAudio() => Instance._StartBackgroundAudio();
+        private void _StartBackgroundAudio() => backgroundAudio.Play();
+        public static void StopBackgroundAudio() => Instance._StopBackgroundAudio();
+        private void _StopBackgroundAudio() => backgroundAudio.Stop();
+        public static void PauseBackgroundAudio() => Instance._PauseBackgroundAudio();
+        private void _PauseBackgroundAudio() => StartCoroutine(AudioHelper.FadeOut(backgroundAudio, _fadeOutTime, backgroundAudio.Pause));
+        public static void ResumeBackgroundAudio() => Instance._ResumeBackgroundAudio();
+        private void _ResumeBackgroundAudio()
         {
-            removeRockSound.Play();
+            if (backgroundAudio.isPlaying) return;
+            StartCoroutine(AudioHelper.FadeIn(backgroundAudio, Instance._fadeInTime, backgroundAudio.UnPause));
         }
+
+        public static void RemoveRock() => Instance._RemoveRock();
+        private void _RemoveRock() => removeRockSound.Play();
 
         public static void GoldFound() => Instance._GoldFound();
-        private void _GoldFound() 
-        {
-            goldFoundSound.Play();
-        }
+        private void _GoldFound() => goldFoundSound.Play();
 
         public static void ItemFound() => Instance._ItemFound();
-        private void _ItemFound() 
-        {
-            itemFoundSound.Play();
-        }
+        private void _ItemFound() => itemFoundSound.Play();
 
         public static void Inspection() => Instance._Inspection();
-        private void _Inspection()
-        {
-            inspectionSound.Play();
-        }
+        private void _Inspection() => inspectionSound.Play();
 
         public static void ArrowAttack() => Instance._ArrowAttack();
-        private void _ArrowAttack() 
-        {
-            arrowAttackSound.Play();
-        }
+        private void _ArrowAttack() => arrowAttackSound.Play();
 
         public static void Error() => Instance._Error();
         public void _Error()
---
+++ Descension/Assets/Scripts/Util/Audio/AudioHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Util.Audio
+{
+    public static class AudioHelper
+    {
+        public static IEnumerator FadeOut(AudioSource audioSource, float fadeTime, Action onStop = null)
+        {
+            var startVolume = audioSource.volume;
+            while (audioSource.volume > 0)
+            {
+                audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+                yield return null;
+            }
+
+            if (onStop == null)
+                audioSource.Stop();
+            else
+                onStop();
+        }
+
+        public static IEnumerator FadeIn(AudioSource audioSource, float fadeTime, Action onStart = null)
+        {
+            if (onStart == null)
+                audioSource.Play();
+            else
+                onStart();
+            audioSource.volume = 0f;
+            while (audioSource.volume < 1)
+            {
+                audioSource.volume += Time.deltaTime / fadeTime;
+                yield return null;
+            }
+        }
+    }
+}
