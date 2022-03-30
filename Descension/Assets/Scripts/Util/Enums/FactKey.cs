b32fc725fbe929775fdb0932ebb385cca8e806c6
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-03-30 12:55:55 -0600

    Safeguard Fact Keys

--- Descension/Assets/Scripts/Util/Enums/FactKey.cs
+++ Descension/Assets/Scripts/Util/Enums/FactKey.cs
@@ -5,37 +5,37 @@ namespace Util.Enums
     [Serializable]
     public enum FactKey
     {
-        None,
+        None = 0,
 
         // Items
-        HasSeenPick,
-        HasSeenTorch,
-        HasSeenSword,
-        HasSeenBow,
-        HasSeenArrow,
-        HasSeenRope,
-        HasSeenHealthPotion,
-        HasSeenGold,
-        HasSeenExplosives,
-        HasSeenTrigger,
-        HasSeenTimer,
+        HasSeenPick = 1,
+        HasSeenTorch = 2,
+        HasSeenSword = 3,
+        HasSeenBow = 4,
+        HasSeenArrow = 5,
+        HasSeenRope = 6,
+        HasSeenHealthPotion = 7,
+        HasSeenGold = 8,
+        HasSeenExplosives = 9,
+        HasSeenTrigger = 10,
+        HasSeenTimer = 11,
 
         // Enemies
-        NumKilledEnemy1,
-        NumKilledEnemy2,
-        NumKilledEnemy3,
+        NumKilledEnemy1 = 12,
+        NumKilledEnemy2 = 13,
+        NumKilledEnemy3 = 14,
 
         // World
-        HasMetShopkeeper,
-        NumItemsPurchased,
-        HasFoundNote1,
-        HasFoundNote2,
-        HasFoundNote3,
-        HasFoundNote4,
-        HasFoundNote5,
-        HasFoundNote6,
-        HasFoundJournal1,
-        HasFoundJournal2,
-        HasFoundJournal3,
+        HasMetShopkeeper = 15,
+        NumItemsPurchased = 16,
+        HasFoundNote1 = 17,
+        HasFoundNote2 = 18,
+        HasFoundNote3 = 19,
+        HasFoundNote4 = 20,
+        HasFoundNote5 = 21,
+        HasFoundNote6 = 22,
+        HasFoundJournal1 = 23,
+        HasFoundJournal2 = 24,
+        HasFoundJournal3 = 25,
     }
 }
